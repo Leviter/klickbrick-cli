22eea123ff4f4afaa5f3dcb15cc008cd608bb3af
Marcel van den Brink <m.vandenbrink@bva-auctions.com>
2020-10-12 09:42:16 +0200

    Made the hello command easier by specifying a default for the name parameter

--- klickbrick_cli/hello.py
+++ klickbrick_cli/hello.py
@@ -5,23 +5,19 @@ Purpose: Say hello
 """
 
 import argparse
-import sys
 
 
 def get_args():
     """Get the command-line arguments"""
 
     parser = argparse.ArgumentParser(description='Say hello')
-    parser.add_argument('-n', '--name', metavar='name', help='name to greet')
+    parser.add_argument('-n', '--name', metavar='name', default="world", help='name to greet')
     return parser.parse_args()
 
 
 def main():
-    if not len(sys.argv) > 1:
-        print('hello world')
-    else:
-        args = get_args()
-        print('hello ' + args.name)
+    args = get_args()
+    print('hello ' + args.name)
 
 
 if __name__ == '__main__':
