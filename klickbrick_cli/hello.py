b8c5829ff7e368177bc41bebfda152a4e45929a9
Marcel van den Brink <m.vandenbrink@bva-auctions.com>
2020-10-12 14:40:53 +0200

    Refactored 'hello', so that it is easier to unit test

--- klickbrick_cli/hello.py
+++ klickbrick_cli/hello.py
@@ -5,19 +5,24 @@ Purpose: Say hello
 """
 
 import argparse
+import sys
 
 
-def get_args():
+def get_args(args):
     """Get the command-line arguments"""
-
     parser = argparse.ArgumentParser(description='Say hello')
     parser.add_argument('-n', '--name', metavar='name', default="world", help='name to greet')
-    return parser.parse_args()
+    return parser.parse_args(args)
+
+
+def get_greeting(arguments):
+    """Get the string that can be used to print on screen"""
+    args = get_args(arguments)
+    return 'hello ' + args.name
 
 
 def main():
-    args = get_args()
-    print('hello ' + args.name)
+    print(get_greeting(sys.argv[1:]))
 
 
 if __name__ == '__main__':
---
+++ tests/test_hello.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+
+from klickbrick_cli import hello
+
+
+class Test(TestCase):
+    def test_get_default_greeting(self):
+        self.assertEqual(hello.get_greeting([]), "hello world")
+
+    def test_get_named_greeting(self):
+        self.assertEqual(hello.get_greeting(['--name', 'Charlie']), "hello Charlie")
+
+    def test_get_empty_name_greeting(self):
+        self.assertEqual(hello.get_greeting(['--name', '']), "hello ")
+
+    def test_get_args_name(self):
+        parsed_arguments = hello.get_args(['--name', 'Michael'])
+        self.assertEqual(parsed_arguments.name, 'Michael')
